10947b230e4b66e5eae98700e0f537b81ee4412f
Saravuth Ruangsutham <saravuth.r@ava.fund>
2021-08-02 12:28:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# med4all-db-schema
+database schema and migration
+
+## PR checker
+PR checker will get the migration info for tables
---
+++ migration/V1_0__create_address.sql
@@ -0,0 +1,40 @@
+
+CREATE TABLE IF NOT EXISTS "patient" (
+    "patient_id" integer PRIMARY KEY,
+    "name" varchar(1000),
+    "age" integer,
+    "weight_kg": integer;
+    "height_cm": integer;
+    "certificate_id" varchar(50),
+    "certificate_type_id" integer,
+    "certificate_picture_url" varchar(2083),
+    "covid_test_picture_url" varchar(2083),
+    "medical_info" JSONB,
+    "last_modified_by" varchar(100),
+    "last_modified_when" timestamptz,
+    "created_by" varchar(100),
+    "created_when" timestamptz
+);
+
+CREATE TABLE IF NOT EXISTS "certificate_type" (
+    "certificate_type_id" integer PRIMARY KEY,
+    "certificate_type" varchar(100),
+    "last_modified_by" varchar(100),
+    "last_modified_when" timestamptz,
+    "created_by" varchar(100),
+    "created_when" timestamptz
+);
+
+CREATE TABLE IF NOT EXISTS "address" (
+    "address_id" integer PRIMARY KEY,
+    "patient_id" integer,
+    "address" varchar(5000),
+    "district" varchar(200),
+    "province" varchar(200),
+    "zipCode" integer,
+    "last_modified_by" varchar(100),
+    "last_modified_when" timestamptz,
+    "created_by" varchar(100),
+    "created_when" timestamptz
+);
+
