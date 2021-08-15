4e8cd9d542c02686eb311be7b0d8909d49a8029b
kkrittipong <66406112+kkrittipong@users.noreply.github.com>
2021-08-15 14:13:11 +0700

    removed doctor related, refactored table

--- migration/V1_0__create_tables.sql
+++ migration/V1_0__create_tables.sql
@@ -10,16 +10,7 @@ CREATE TABLE IF NOT EXISTS "patient" (
     "certificate_picture_url" varchar(2083),
     "covid_test_picture_url" varchar(2083),
     "medical_info" JSONB,
-    "diagnostic_status_id" integer,
-    "last_modified_by" varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by" varchar(100),
-    "created_when" timestamptz
-);
-
-CREATE TABLE IF NOT EXISTS "diagnostic_status" (
-    "diagnostic_status_id" SERIAL PRIMARY KEY,
-    "diagnostic_status" varchar(100),
+    "volunteer_id" integer NOT NULL,
     "last_modified_by" varchar(100),
     "last_modified_when" timestamptz,
     "created_by" varchar(100),
@@ -48,50 +39,6 @@ CREATE TABLE IF NOT EXISTS "address" (
     "created_when" timestamptz
 );
 
-CREATE TABLE IF NOT EXISTS "doctor" (
-    "doctor_id" SERIAL PRIMARY KEY,
-    "name" varchar(1000) NOT NULL,
-    "medical_license_id" varchar(50) UNIQUE NOT NULL,
-    "mobile_phone_number" varchar(50) UNIQUE NOT NULL,
-    "is_active" boolean,
-    "last_modified_by" varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by" varchar(100),
-    "created_when" timestamptz
-);
-
-CREATE TABLE IF NOT EXISTS "doctor_token" (
-    "doctor_token_id" SERIAL PRIMARY KEY,
-    "token" text UNIQUE NOT NULL,
-    "valid_until" timestamptz,
-    "last_modified_by" varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by" varchar(100),
-    "created_when" timestamptz
-);
-
-
-CREATE TABLE IF NOT EXISTS "patient_result" (
-    "patient_result_id" SERIAL PRIMARY KEY,
-    "patient_id" integer,
-    "doctor_id" integer,
-    "is_approved" boolean,
-    "reject_reason_id" integer,
-    "remark" text,
-    "last_modified_by" varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by" varchar(100),
-    "created_when" timestamptz
-);
-
-CREATE TABLE IF NOT EXISTS "reject_reason" (
-    "reject_reason_id" SERIAL PRIMARY KEY,
-    "reject_reason" varchar(100),
-    "last_modified_by" varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by" varchar(100),
-    "created_when" timestamptz
-);
 
 CREATE TABLE IF NOT EXISTS "volunteer" (
     "volunteer_id" SERIAL PRIMARY KEY,
