1209da03333dc15658e745a91e08eb21f3ff2b8e
Orm Saravuth <85147834+saravuthr@users.noreply.github.com>
2021-08-17 14:08:06 +0700

    Update V1_0__create_tables.sql

--- migration/V1_0__create_tables.sql
+++ migration/V1_0__create_tables.sql
@@ -1,96 +1,96 @@
 CREATE TABLE IF NOT EXISTS "patient"
-(
-    "patient_id"          SERIAL PRIMARY KEY,
-    "certificate_id"      varchar(200) NOT NULL,
-    "certificate_type"    integer      NOT NULL,
-    "name"                varchar(200) NOT NULL,
-    "surname"             varchar(200) NOT NULL,
-    "gender"              integer      NOT NULL,
-    "age_year"            integer      NOT NULL,
-    "patient_phone"       varchar(200) NOT NULL,
-    "custodian_phone"     varchar(200),
-    "weight_kg"           integer      NOT NULL,
-    "height_cm"           integer      NOT NULL,
-    "medical_info"        JSONB        NOT NULL,
-    "check_in_date"       timestamptz,
-    "check_out_date"      timestamptz,
-    "patient_data_source" integer,
-    "admitted_to"         varchar(200),
-    "health_coverage"     integer,
-    "line_id"             varchar(200),
-    "home_town"           integer,
-    "equipments"          text,
-    "volunteer_id"        integer      NOT NULL,
-    "last_modified_by"    varchar(100),
-    "last_modified_when"  timestamptz,
-    "created_by"          varchar(100),
-    "created_when"        timestamptz
-);
+  (
+     "patient_id"              SERIAL PRIMARY KEY,
+     "certificate_id"          VARCHAR(200) NOT NULL,
+     "certificate_type"        INTEGER NOT NULL,
+     "name"                    VARCHAR(200) NOT NULL,
+     "surname"                 VARCHAR(200) NOT NULL,
+     "gender"                  INTEGER NOT NULL,
+     "age_year"                INTEGER NOT NULL,
+     "patient_phone"           VARCHAR(200) NOT NULL,
+     "custodian_phone"         VARCHAR(200),
+     "weight_kg"               INTEGER NOT NULL,
+     "height_cm"               INTEGER NOT NULL,
+     "medical_info"            JSONB NOT NULL,
+     "check_in_date"           TIMESTAMPTZ,
+     "check_out_date"          TIMESTAMPTZ,
+     "patient_data_source"     INTEGER,
+     "admitted_to"             VARCHAR(200),
+     "health_coverage"         INTEGER,
+     "line_id"                 VARCHAR(200),
+     "home_town"               INTEGER,
+     "equipments"              TEXT,
+     "volunteer_id"            INTEGER NOT NULL,
+     "certificate_picture_url" VARCHAR(2083),
+     "covid_test_picture_url"  VARCHAR(2083),
+     "last_modified_by"        VARCHAR(100),
+     "last_modified_when"      TIMESTAMPTZ,
+     "created_by"              VARCHAR(100),
+     "created_when"            TIMESTAMPTZ
+  );
 
 CREATE TABLE IF NOT EXISTS "certificate_type"
-(
-    "certificate_type_id" SERIAL PRIMARY KEY,
-    "certificate_type"    varchar(100),
-    "last_modified_by"    varchar(100),
-    "last_modified_when"  timestamptz,
-    "created_by"          varchar(100),
-    "created_when"        timestamptz
-);
+  (
+     "certificate_type_id" SERIAL PRIMARY KEY,
+     "certificate_type"    VARCHAR(100),
+     "last_modified_by"    VARCHAR(100),
+     "last_modified_when"  TIMESTAMPTZ,
+     "created_by"          VARCHAR(100),
+     "created_when"        TIMESTAMPTZ
+  );
 
 CREATE TABLE IF NOT EXISTS "address"
-(
-    "address_id"         SERIAL PRIMARY KEY,
-    "patient_id"         integer NOT NULL,
-    "province_code"      integer,
-    "district_code"      integer,
-    "sub_district_code"  integer,
-    "moo"                varchar(200),
-    "road"               varchar(200),
-    "alley"              varchar(200),
-    "soi"                varchar(200),
-    "village"            varchar(200),
-    "bangkok_zone_code"  integer,
-    "zip_code"           integer,
-    "building"           varchar(200),
-    "note"               varchar(1000),
-    "last_modified_by"   varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by"         varchar(100),
-    "created_when"       timestamptz
-);
-
+  (
+     "address_id"         SERIAL PRIMARY KEY,
+     "patient_id"         INTEGER NOT NULL,
+     "province_code"      INTEGER,
+     "district_code"      INTEGER,
+     "sub_district_code"  INTEGER,
+     "moo"                VARCHAR(200),
+     "road"               VARCHAR(200),
+     "alley"              VARCHAR(200),
+     "soi"                VARCHAR(200),
+     "village"            VARCHAR(200),
+     "bangkok_zone_code"  INTEGER,
+     "zip_code"           INTEGER,
+     "building"           VARCHAR(200),
+     "note"               VARCHAR(1000),
+     "last_modified_by"   VARCHAR(100),
+     "last_modified_when" TIMESTAMPTZ,
+     "created_by"         VARCHAR(100),
+     "created_when"       TIMESTAMPTZ
+  );
 
 CREATE TABLE IF NOT EXISTS "volunteer"
-(
-    "volunteer_id"        SERIAL PRIMARY KEY,
-    "name"                varchar(1000)      NOT NULL,
-    "volunteer_team_id"   integer            NOT NULL,
-    "mobile_phone_number" varchar(50) UNIQUE NOT NULL,
-    "is_active"           boolean,
-    "last_modified_by"    varchar(100),
-    "last_modified_when"  timestamptz,
-    "created_by"          varchar(100),
-    "created_when"        timestamptz
-);
+  (
+     "volunteer_id"        SERIAL PRIMARY KEY,
+     "name"                VARCHAR(1000) NOT NULL,
+     "volunteer_team_id"   INTEGER NOT NULL,
+     "mobile_phone_number" VARCHAR(50) UNIQUE NOT NULL,
+     "is_active"           BOOLEAN,
+     "last_modified_by"    VARCHAR(100),
+     "last_modified_when"  TIMESTAMPTZ,
+     "created_by"          VARCHAR(100),
+     "created_when"        TIMESTAMPTZ
+  );
 
 CREATE TABLE IF NOT EXISTS "volunteer_token"
-(
-    "volunteer_token_id" SERIAL PRIMARY KEY,
-    "token"              text UNIQUE NOT NULL,
-    "valid_until"        timestamptz,
-    "last_modified_by"   varchar(100),
-    "last_modified_when" timestamptz,
-    "created_by"         varchar(100),
-    "created_when"       timestamptz
-);
+  (
+     "volunteer_token_id" SERIAL PRIMARY KEY,
+     "token"              TEXT UNIQUE NOT NULL,
+     "valid_until"        TIMESTAMPTZ,
+     "last_modified_by"   VARCHAR(100),
+     "last_modified_when" TIMESTAMPTZ,
+     "created_by"         VARCHAR(100),
+     "created_when"       TIMESTAMPTZ
+  );
 
 CREATE TABLE IF NOT EXISTS "volunteer_team"
-(
-    "volunteer_team_id"   SERIAL PRIMARY KEY,
-    "volunteer_team_name" varchar(1000),
-    "last_modified_by"    varchar(100),
-    "last_modified_when"  timestamptz,
-    "created_by"          varchar(100),
-    "created_when"        timestamptz
-);
-
+  (
+     "volunteer_team_id"   SERIAL PRIMARY KEY,
+     "volunteer_team_name" VARCHAR(1000),
+     "last_modified_by"    VARCHAR(100),
+     "last_modified_when"  TIMESTAMPTZ,
+     "created_by"          VARCHAR(100),
+     "created_when"        TIMESTAMPTZ
+  ); 
