1d5deeebc702fd22c2fd39480c020bc19986e2d9
Natechawin <natechawin@gmail.com>
2021-09-14 19:35:01 +0700

    add alter table to every rename line

--- migration/V1_3__alter_address_table.sql
+++ migration/V1_3__alter_address_table.sql
@@ -1,8 +1,13 @@
 ALTER TABLE address
 ADD COLUMN address_detail VARCHAR(1000)
-RENAME COLUMN province_code TO province
-RENAME COLUMN district_code TO district
-RENAME COLUMN sub_district_code TO sub_district
+
+ALTER TABLE address RENAME COLUMN province_code TO province
+ALTER TABLE address RENAME COLUMN district_code TO district
+ALTER TABLE address RENAME COLUMN sub_district_code TO sub_district
+ALTER TABLE address RENAME COLUMN bangkok_zone_code TO bangkok_zone
+
+ALTER TABLE address
 ALTER COLUMN province VARCHAR(200)
 ALTER COLUMN district VARCHAR(200)
 ALTER COLUMN sub_district VARCHAR(200);
+ALTER COLUMN bangkok_zone VARCHAR(200);
